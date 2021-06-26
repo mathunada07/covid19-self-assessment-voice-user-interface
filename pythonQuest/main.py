8b4ed333a8cfcdddea1fe49f377ed1eec776a7e8
mathunada07 <mathunada07@gmail.com>
2021-06-26 05:49:37 -0400

    Rough Prototype of Auditory Covid 19 Self Assessment
    Two functions, one for simple yes/no answers and one for emergency yes/no answers, exist for the questionnaire. Google Speech API picks up on "yes" and "no" and reacts accordingly. For simple questions, an "unsafe" answer changes the final result to a stay-at-home order. For emergency questions, an undesirable answer leads to advice to call 911 and the program is stopped. To communicate with the user, pyttsx 3 is used to convert text to speech.

--- pythonQuest/main.py
+++ pythonQuest/main.py
@@ -1,16 +1,58 @@
-# This is a sample Python script.
+import sys
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import pyaudio
+import speech_recognition as sr
+import pyttsx3
+import sys
+recognizer = sr.Recognizer()
+speaker = pyttsx3.init()
 
+bad_answer = 0
+emergency_number = 0
+speaker.say("Welcome to the Covid 19 self-assessment. You will be asked a series of questions. Please answer yes or no.")
+speaker.runAndWait()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def yes_no_question(question):
+    global emergency_number
+    if emergency_number == 0:
+        with sr.Microphone() as source:
+            speaker.say(question)
+            speaker.runAndWait()
+            audio = recognizer.listen(source)
 
+            try:
+                text = recognizer.recognize_google(audio)
+                if text == "yes":
+                    global bad_answer
+                    bad_answer = bad_answer + 1
+                    print(bad_answer)
+            except:
+                print("please answer yes or no")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def emergency_question(question):
+    global emergency_number
+    if emergency_number == 0:
+        with sr.Microphone() as source:
+            speaker.say(question)
+            speaker.runAndWait()
+            audio = recognizer.listen(source)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+            try:
+                text = recognizer.recognize_google(audio)
+                if text == "yes":
+                    speaker.say("You may be in a crisis. Please call 911 immediately.")
+                    speaker.runAndWait()
+                    emergency_number += 1
+            except:
+                print("please answer yes or no")
+
+yes_no_question("Are you experiencing shortness of breath?")
+emergency_question("Are you experiencing severe chest pain?")
+yes_no_question("Have any family members been infected with Covid 19 in the last 14 days?")
+
+if bad_answer > 0 and emergency_number == 0:
+    speaker.say("You are at high risk of covid 19. Please stay home for 14 days and call 911 if an emergency arises.")
+    speaker.runAndWait()
+elif bad_answer == 0 and emergency_number == 0:
+    speaker.say("you are declared safe to go outside!")
+    speaker.runAndWait()
